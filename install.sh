293b0e29adea06b01530624661322cb3b3db85f0
lxgr <lxgr@protonmail.com>
2020-09-24 13:58:21 +0200

    patch

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ cp ./os-release /etc/
 cp ./os-release /usr/lib/
 cp ./lsb-release /etc/
 cp ./hipsterlinux.png /usr/share/pixmaps
+if [[ $(find /usr/bin/neofetch) ]]
+then
+	patch /usr/bin/neofetch ./neofetch_hipster.patch
+fi
